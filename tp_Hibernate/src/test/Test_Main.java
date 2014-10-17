65aabf115866bb889d61d335d4f6b9cf918671ce
zarrouk <zarrouk_01@homail.fr>
2014-10-17 03:16:05 +0200

    First change

--- tp_Hibernate/src/test/Test_Main.java
+++ tp_Hibernate/src/test/Test_Main.java
@@ -6,6 +6,7 @@ import manager.PersonneManager;
 public class Test_Main {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		PersonneManager pm = new PersonneManager();
 		pm.ajoutPersonne("Godhben", "Marouen", "062458798", "rou@rou.com");
 		pm.ajoutPersonne("Godhben2", "Marouen2", "062458798", "rou@rou.com2");
